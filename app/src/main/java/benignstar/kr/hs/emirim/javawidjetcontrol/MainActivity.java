1734603b76576c3ee36a0e24c01ff1a376e3fe36
benignstar <che0009@naver.com>
2017-03-23 20:08:25 +0900

    #2 change widget in the java

--- app/src/main/java/benignstar/kr/hs/emirim/javawidjetcontrol/MainActivity.java
+++ app/src/main/java/benignstar/kr/hs/emirim/javawidjetcontrol/MainActivity.java
@@ -1,7 +1,10 @@
 package benignstar.kr.hs.emirim.javawidjetcontrol;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text1=(TextView)findViewById(R.id.text1);
+        TextView text2=(TextView)findViewById(R.id.text2);
+        TextView text3=(TextView)findViewById(R.id.text3);
+        TextView text4=(TextView)findViewById(R.id.text4);
+
+        text1.setText("First TextView");
+        text1.setTextSize(30);
+        text1.setBackgroundColor(Color.rgb(255, 0, 255));
+        text1.setTextColor(Color.WHITE);
+
+        text2.setText(R.string.text2_str);
+        text2.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC);
+
+        text3.setTextScaleX(50);
+        text3.setText("Third TextView");
+
+        text4.setBackgroundColor(Color.RED);
+
+
     }
 }
